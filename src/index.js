949323ab26c1ef26e7736825ae86b80f62160d57
Rowa <jrowah@gmail.com>
2023-01-23 14:30:33 +0300

    lifts up state

--- src/index.js
+++ src/index.js
@@ -4,32 +4,47 @@ import "./index.css";
 
 //class component example; The Square component that renders a single <button>
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    //givers the Square component a state (private to this state) and sets its value to null
-    //for the square to remember that it got clicked and fill with an "X" mark, we use state which components use to remember things
-    //current state value set to null
-    this.state = {
-      value: null,
-    };
-  }
+  //deleted square constructor since it no longer keeps track of the game's state
+  // constructor(props) {
+  //   super(props);
+  //   //givers the Square component a state (private to this state) and sets its value to null
+  //   //for the square to remember that it got clicked and fill with an "X" mark, we use state which components use to remember things
+  //   //current state value set to null
+  //   this.state = {
+  //     value: null,
+  //   };
+  // }
   render() {
     return (
       <button
         className="square"
         //we’re passing a function as the onClick prop
         //state value changes when the button is clicked
-        onClick={() => this.setState({ value: "X" })}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  //lifts up state so that the board component can communicate with each square and pass down props to them
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    //gives each square a value of either "X", "O", or null
+    return (
+      <Square
+        value={this.state.squares[i]}
+        //When a Square is clicked, the onClick function provided by the Board is called
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
