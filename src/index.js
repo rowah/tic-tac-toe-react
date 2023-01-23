fe10c18fa4c4133e22164f04336510afc1959d9d
Rowa <jrowah@gmail.com>
2023-01-23 11:31:25 +0300

    sets state and calls setState

--- src/index.js
+++ src/index.js
@@ -4,8 +4,26 @@ import "./index.css";
 
 //class component example; The Square component that renders a single <button>
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    //givers the Square component a state (private to this state) and sets its value to null
+    //for the square to remember that it got clicked and fill with an "X" mark, we use state which components use to remember things
+    //current state value set to null
+    this.state = {
+      value: null,
+    };
+  }
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        //we’re passing a function as the onClick prop
+        //state value changes when the button is clicked
+        onClick={() => this.setState({ value: "X" })}
+      >
+        {this.state.value}
+      </button>
+    );
   }
 }
 
